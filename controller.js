e9dfe95af51d01c1f13a6b44b7b5b0f908e4fe05
LeCodeBae <billivank_96@yahoo.co.id>
2017-04-07 21:09:35 +0700

    release 1 beres

--- controller.js
+++ controller.js
@@ -1,2 +1,55 @@
 "use strict"
-// write your code here
+var View = require('./view.js')
+var Model = require('./model.js')
+if(process.argv[2] === undefined){
+  var m = new Model(process.argv[2])
+} else{
+  var m = new Model()
+}
+
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+class Controller{
+  constructor(){
+    this.qna = m.data
+    this.num = 0
+  }
+  quiz(){
+    View.line()
+    if(this.num<this.qna.length){
+      View.definition()
+      View.question(this.qna,this.num)
+      this.answer(this.num)
+    } else {
+      View.congratulations();
+      rl.close();
+    }
+  }
+  answer(num){
+    View.line()
+    rl.question('Guess:', (answer) => {
+      if(this.checkAnswer(num,answer)===false){
+        View.incorrect()
+        this.answer(num);
+      } else {
+        View.correct()
+        this.num++;
+        this.quiz();
+      }
+    });
+  }
+  checkAnswer(q,a){
+      return a.toLowerCase() === this.qna[q].term.toLowerCase();
+  }
+}
+var fc = new Controller();
+if(process.argv[2] === undefined){
+  View.welcome()
+} else{
+  View.welcomeDefined(process.argv[2])
+}
+
+fc.quiz();
--- view.js
+++ view.js
@@ -1,2 +1,27 @@
 "use strict"
-// write your code here
+module.exports = class View{
+  static welcome(){
+    console.log('Welcome to JS Flash Cards. To play, just enter the correct term for each definition. Ready? Go!')
+  }
+  static welcomeDefined(file){
+    console.log(`Welcome to JS Flash Cards. You're using the deck ${file}. \nTo play, just enter the correct term for each definition. Ready? Go!`)
+  }
+  static question(m,q) {
+    console.log(m[q].definition)
+  }
+  static line(){
+    console.log('')
+  }
+  static definition(){
+    console.log('Definition')
+  }
+  static congratulations(){
+    console.log("Congratulations!")
+  }
+  static incorrect(){
+    console.log("Incorrect! Try again.")
+  }
+  static correct(){
+    console.log("Correct!")
+  }
+}
