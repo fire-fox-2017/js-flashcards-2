baeb1677cc41627e81c4daef8c6c7d7a319083c2
haryanawisnu <arya.wisnu05@gmail.com>
2017-04-07 16:08:11 +0700

    done

--- controller.js
+++ controller.js
@@ -1,2 +1,48 @@
 "use strict"
 // write your code here
+import Model from "./model.js"
+import View from "./view.js"
+class Controller {
+    constructor() {
+        this.model = new Model();
+        this.view = new View();
+    }
+    start() {
+        this.model.getData();
+        let i = 0;
+        this.ask(i);
+    }
+    ask(i) {
+        if (i < this.model.listData.length && this.model.listStatus[i] != "DONE") {
+            let strAsk = `${this.model.listData[i].definition}  \nGuess : `;
+            this.model.rl.question(strAsk, (answer) => {
+                if (answer.toUpperCase() == "SKIP") {
+                    this.view.skip();
+                    this.model.listStatus[i] = "SKIP";
+                    i++;
+                    this.model.fault = 0;
+                    return this.ask(i);
+                } else if (answer.toUpperCase() == this.model.listData[i].term) {
+                    this.view.correct();
+                    this.model.listStatus[i] = "DONE";
+                    i++;
+                    this.model.fault = 0;
+                    return this.ask(i);
+                } else {
+                    this.model.fault += 1;
+                    this.view.incorrect(this.model.fault);
+                    return this.ask(i);
+                }
+            });
+        } else {
+            if (this.model.listStatus.includes("SKIP")) {
+                i = 0;
+                return this.ask(i);
+            } else {
+                this.view.finish();
+                this.model.rl.close();
+            }
+        }
+    }
+}
+export default Controller
--- model.js
+++ model.js
@@ -1,2 +1,26 @@
 "use strict"
 // write your code here
+class Model {
+    constructor() {
+        this.listData = [];
+        this.rl = {};
+        this.fault = 0;
+        this.listStatus = [];
+    }
+    getData() {
+        const fs = require('fs');
+        const readline = require('readline');
+        this.rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        let strJson = fs.readFileSync('social.json');
+        let str = JSON.parse(strJson);
+        this.listData = str;
+        for (let i = 0; i < str.length; i++) {
+            this.listStatus.push("");
+        }
+    }
+}
+
+export default Model
