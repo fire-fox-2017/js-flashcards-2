13d688fc41a958024e99a5da8643edce4ffedb7b
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-04-07 12:57:19 +0800

    separated model and view, still working on argv

--- model.js
+++ model.js
@@ -1,2 +1,27 @@
 "use strict"
 // write your code here
+
+
+class Model {
+
+  loadJsonFile(file) {
+    // console.log("rudy");
+    var fs = require('fs');
+
+    let data = fs.readFileSync(file)
+      .toString();
+
+    // console.log(data);
+    let json_data = JSON.parse(data);
+    // console.log(json_data);
+    // console.log(json_data[0]);
+    return json_data;
+  }
+}
+
+
+// module.exports = {
+//   Model
+// }
+// export default Model;
+module.exports = Model;
--- view.js
+++ view.js
@@ -1,2 +1,20 @@
 "use strict"
 // write your code here
+
+class Show {
+  printWelcomeMsg() {
+    console.log("Welcome to JS Flash Cards. To Play, just enter the correct term for each definition. Ready? Go!");
+  }
+
+  printCard(card) {
+
+    console.log(`\nDefinition\n${card['definition']}\n`);
+
+  }
+
+  printEndPractice() {
+    console.log(`\nCongrats, you have completed your practice!`);
+  }
+}
+
+module.exports = Show;
