d4dd7ef37b6fe0f7a384d5bf750d4c124a19d647
edim dendy &   "name": null <pair+edimdendidiwangga.zanisis@hacktiv8.com>
2017-04-08 17:13:16 +0700

    flashcar2 - edim

--- model.js
+++ model.js
@@ -1,2 +1,12 @@
 "use strict"
 // write your code here
+import fs from 'fs'
+class Model {
+  constructor(dataJson) {
+    this.data = dataJson || 'social.json';
+  }
+  getData() {
+    return JSON.parse(fs.readFileSync(this.data, 'utf-8')) // return object
+  }
+}
+export default Model
