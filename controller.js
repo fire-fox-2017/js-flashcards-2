b5e31e2f970ccdbec2378a9b768a3259d6146a0b
Bhirawa Mbani <bhirmbani@gmail.com>
2017-04-08 21:08:47 +0700

    easy mode implementation

--- controller.js
+++ controller.js
@@ -3,13 +3,68 @@
 import Model from './model';
 import View from './view';
 
-const rl = require('readline');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Guess '
+});
 
 class Controller {
   constructor(){
     this.model = new Model();
     this.view = new View();
+    this.data = this.model.getData(); // obj
+    this.wrong = 0;
+    this.correct = 0;
+    this.questIdx = 0;
   }
+
+  welcome() {
+    this.view.welcomeMsg();
+    rl.on('line', (userInput) => {
+      if(userInput === 'hint') {
+        rl.close();
+        this.play(this.easyMode());
+      } else {
+        this.play();
+      }
+    });
+  }
+
+  easyMode() {
+    return 'easy';
+  }
+
+  play(isEasy = 'normal') {
+    if(isEasy === 'easy') {
+      rl.on('line', (userInput) => {
+      if(this.data[questIdx].term.toLowerCase() === userInput.toLowerCase()) {
+        this.correct();
+      }
+    });
+    }
+    
+  }
+
+  correct() {
+    this.correct++;
+    this.questIdx++;
+    this.view.correctMsg(userInput);
+  }
+
+  wrong() {
+
+  }
+
+  skip() {
+
+  }
+  
+  showQuestion() {
+
+  }
+
 }
 
 export default Controller;
--- view.js
+++ view.js
@@ -5,6 +5,26 @@ class View {
   constructor() {
 
   }
+
+  welcomeMsg() {
+    console.log(`======Welcome to JS Flashcards 2======\n======ketik 'hint' untuk easy mode======`);
+  }
+
+  winMsg() {
+
+  }
+
+  correctMsg(userInput) {
+    console.log(`Jawaban '${userInput}' benar. next question`)
+  }
+
+  wrongMsg() {
+
+  }
+
+  skipMsg() {
+
+  }
 }
 
 export default View;
