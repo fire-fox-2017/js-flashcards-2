6210809af475637dc4b2ee61bfaf212b7f2c82f0
oscarhermawan <franky1790@gmail.com>
2017-04-08 21:02:24 +0700

    flashcards-2-done

--- flashcards.js
+++ flashcards.js
@@ -1,2 +1,22 @@
 "use strict"
 // write your code here
+
+import Controller from './controller'
+import View from './view'
+
+
+class Flashcard{
+  static mulai(input){
+    if(input.length<=2){
+      console.log(View.pilihSoal());
+    }
+    else{
+      let controller = new Controller(input[2]);
+      controller.play();
+    }
+  }
+}
+
+
+let input = process.argv
+Flashcard.mulai(input);
--- view.js
+++ view.js
@@ -1,2 +1,28 @@
 "use strict"
+
+class View{
+
+  static welcome(){
+
+  }
+
+  static pilihSoal(){
+    return `Tuliskan jenis soal permainan : \n* babel-node flashcards.js bola.json\n* babel-node flashcards.js social.json`
+  }
+
+  static benarJawaban(){
+    return `Jawaban anda benar\n`
+  }
+
+  static salahJawaban(){
+    return `JAWABAN SALAH`
+  }
+
+  static selesai(salah){
+    return `Semua Soal telah terjawab dan Total anda salah sebanyak ${salah}`
+  }
+}
+
+
+export default View
 // write your code here
