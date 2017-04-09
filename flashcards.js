a598243873d9b66cb96eb1abc44c8ea922dada71
stedyyulius <stedyyulius@gmail.com>
2017-04-09 21:10:59 +0700

    tolong windowsku error T.T

--- flashcards.js
+++ flashcards.js
@@ -4,9 +4,9 @@ import Controller from "./controller.js"
 
 let argv = process.argv;
 let game = new Controller(argv[2]);
-// game.starter();
-// game.init()
-console.log(game.quiz)
+ game.starter();
+ game.init()
+//console.log(game.quiz)
 
 // if(argv[2] === "Quiz_1"){
 //   game.init()
